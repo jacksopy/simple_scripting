559d8a54316e711bbd3bb106b1a25cdcca7c7a38
jacksopy <148472316+jacksopy@users.noreply.github.com>
2025-05-18 15:05:24 +0200

    Add files via upload

---
+++ code/main_osint.sh
@@ -0,0 +1,38 @@
+echo " ▒█████    ██████  ██▓ ███▄    █ ▄▄▄█████▓    ▄████▄    ██████  ██▓ ███▄    █ ▄▄▄█████▓"
+echo "▒██▒  ██▒▒██    ▒ ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒   ▒██▀ ▀█  ▒██    ▒ ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒"
+echo "▒██░  ██▒░ ▓██▄   ▒██▒▓██  ▀█ ██▒▒ ▓██░ ▒░   ▒▓█    ▄ ░ ▓██▄   ▒██▒▓██  ▀█ ██▒▒ ▓██░ ▒░"
+echo "▒██   ██░  ▒   ██▒░██░▓██▒  ▐▌██▒░ ▓██▓ ░    ▒▓▓▄ ▄██▒  ▒   ██▒░██░▓██▒  ▐▌██▒░ ▓██▓ ░ "
+echo "░ ████▓▒░▒██████▒▒░██░▒██░   ▓██░  ▒██▒ ░    ▒ ▓███▀ ░▒██████▒▒░██░▒██░   ▓██░  ▒██▒ ░ "
+echo "░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░▓  ░ ▒░   ▒ ▒   ▒ ░░      ░ ░▒ ▒  ░▒ ▒▓▒ ▒ ░░▓  ░ ▒░   ▒ ▒   ▒ ░░"
+echo "  ░ ▒ ▒░ ░ ░▒  ░ ░ ▒ ░░ ░░   ░ ▒░    ░         ░  ▒   ░ ░▒  ░ ░ ▒ ░░ ░░   ░ ▒░    ░"
+echo "░ ░ ░ ▒  ░  ░  ░   ▒ ░   ░   ░ ░   ░         ░        ░  ░  ░   ▒ ░   ░   ░ ░   ░"
+echo "    ░ ░        ░   ░           ░             ░ ░            ░   ░           ░"
+echo "                                             ░"
+echo "+---------------+--------------+-------------+-----------+"
+echo "+ lookup nazapi / lookup phone / lookup snus / lookup db +" 
+echo "+---------------+--------------+-------------+-----------+"
+echo "+       1       /       2      /      3      /      4    +"
+echo "+---------------+--------------+-------------+-----------+"
+echo "$" && read choix
+if [ $choix == 2 ]; then
+    clear
+    cd osint
+    echo "met le numéro a énumerer format +33612345678"
+    read numero
+    bash lookup_db.sh $numero
+elif [ $choix == 1 ]; then
+    clear
+    cd osint 
+    python3 lookupnaz.py
+elif [ $choix == 3 ]; then
+    clear
+    cd osint 
+    python3 snusbase.py
+elif [ $choix == 4 ]; then
+    clear
+    cd osint
+    bash lookup_db.sh
+else
+    clear
+    ./main_osint.sh
+fi                 
---
+++ req.sh
@@ -0,0 +1,18 @@
+chmod +x simple_scripting.sh
+cd code
+chmod +x main_osint.sh
+chmod +x main_pentest.sh
+cd pentest
+chmod +x simple_enum4linux.sh
+chmod +x simple_sqlmap.sh
+chmod +x simple_ffuf.sh
+chmod +x simple_netcat.sh 
+chmod +x simple_searchsploit.sh
+chmod +x simple_nmap.sh
+cd ..
+cd osint
+chmod +x lookup_db.sh
+chmod +x lookup_num.sh
+cd ..
+cd ..
+pip install requests
